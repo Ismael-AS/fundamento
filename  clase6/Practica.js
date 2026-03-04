06514fbb9e5e8aaf858c1b6dc53f39ba49244819
Ismael-AS <anchiasbran@gmail.com>
2026-03-04 17:04:19 -0600

    Add files via upload

---
+++  clase6/Practica.js
@@ -0,0 +1,18 @@
+
+const console1 = false;
+const console2 = true 
+const console3 = false
+
+const lunes = "lunes";
+console.log ("Hoy es miercoles?)" + 
+console.log ("Hoy es lunes?" + console1);
+console.log ("hoy es martes o miercoles?" + )
+console.log ("hoy es miercoles y jueves?" + )
+console.log ("")
+
+true
+false
+
+const num = true
+
+// Amen
---
+++  clase6/main.js
@@ -0,0 +1,45 @@
+console.log("hola");
+const age = 33;
+var number = 10;
+
+var numberFloat = 15.60
+console.log(numberFloat + 10);
+
+let numberWithzero = "15.60";
+console.log(numberWithzero + 10);
+
+let name = "Brandon ";
+
+true
+false 
+
+//numeros 
+
+const suma = 40 + 20 //con () = se prioriza una operacion
+console.log (suma);
+
+//operadores +,-,*, /
+
+//strings
+
+let firstName = "Brandon ";
+const lastname = "anchia";
+
+console.log(name + lastname)
+
+console.error ("Esto es un error!!")
+
+const Class = "Brandon"; 
+
+// Booleans 
+
+true
+false 
+
+const nuM = 10;
+
+//&&
+const num = 10;
+console.log(num > 4)
+
+//||
